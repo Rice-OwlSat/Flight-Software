0c5faf5a6764749739abe06599f96ba7b0c63f1a
JuanCtan <juanantonio.decarcer@gmail.com>
2022-05-22 16:36:50 +0200

    Update euv_sensor_task.py

--- OwlSat - Flight Software/Tasks/euv_sensor_task.py
+++ OwlSat - Flight Software/Tasks/euv_sensor_task.py
@@ -2,9 +2,15 @@ from Tasks.template_task import Task
 import board
 from analogio import AnalogIn
 
+SENSOR1_PIN = board.A0
+SENSOR2_PIN = board.A1
+SENSOR3_PIN = board.A2
+SENSOR4_PIN = board.A4
+SENSOR5_PIN = board.A5
+
 class task(Task):
     priority = 10
-    frequency = 1
+    frequency = 3
     name = 'euv'
     color = 'red'
 
@@ -13,9 +19,11 @@ class task(Task):
     async def main_task(self):
         # take EUV readings
         readings = {
-            'EUV_x': (AnalogIn(board.A1).value * 3.3) / 65536,
-            'EUV_y': 0,
-            'EUV_z': 0,
+            'EUV_+x': (AnalogIn(SENSOR1_PIN).value * 3.3) / 65536,
+            'EUV_-x': (AnalogIn(SENSOR2_PIN).value * 3.3) / 65536,
+            'EUV_+y': (AnalogIn(SENSOR3_PIN).value * 3.3) / 65536,
+            'EUV_-y': (AnalogIn(SENSOR4_PIN).value * 3.3) / 65536,
+            'EUV_+z': (AnalogIn(SENSOR5_PIN).value * 3.3) / 65536
         }
 
         # store them in our cubesat data_cache object
