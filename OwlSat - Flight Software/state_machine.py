cce60d8e0705feff62abf59940b2fc97f0e79b03
JuanCtan <juanantonio.decarcer@gmail.com>
2022-06-15 01:20:24 +0200

    Small tweak to the Match Case switch
    The editor doesnt like match case, idk why. I replaced it with standard if else just in case

--- OwlSat - Flight Software/state_machine.py
+++ OwlSat - Flight Software/state_machine.py
@@ -1,4 +1,3 @@
-from pycubed import *
 
 # State ID Constants
 STOWED = 0
@@ -35,7 +34,6 @@ ALL_TASKS = ["collect_imu_data",
              "write_cache_to_SD"]
 
 
-
 class StateMachine:
     def __init__(self):
         self.state = STOWED
@@ -67,44 +65,42 @@ class StateMachine:
                 self.task_flags[task] = True
 
     def progress_state(self):
-        match self.state:
-            
-            case STOWED: # Cubesat just turned ON
-                self.state = DEPLOYING
+        if self.state == STOWED: # Cubesat just turned ON
+            self.state = DEPLOYING
                 
-            case DEPLOYING: # Cubesat is initialized and running - Antenna must be deployed
-                if self.deployment_successful:
-                    self.state = DETUMBLING
-                elif not self.deployment_successful:
-                    self.state = DEPLOYING
+        elif self.state == DEPLOYING: # Cubesat is initialized and running - Antenna must be deployed
+            if self.deployment_successful:
+                self.state = DETUMBLING
+            elif not self.deployment_successful:
+                self.state = DEPLOYING
                     
-            case DETUMBLING: # Cubesat is detumbling
-                if self.angular_velocity_stable:
-                    self.state = STABLE
-                elif not self.angular_velocity_stable:
-                    self.state = DETUMBLING
+        elif self.state == DETUMBLING: # Cubesat is detumbling
+            if self.angular_velocity_stable:
+                self.state = STABLE
+            elif not self.angular_velocity_stable:
+                self.state = DETUMBLING
                     
-            case STABLE: # Cubessat has detumbled - Must establish comms link
-                if self.angular_velocity_stable and self.battery_check_successful and self.ground_station_link_successful:
-                    self.state = MISSION
-                elif not self.battery_check_successful:
-                    self.state = RECOVERY
-                elif not self.angular_velocity_stable:
-                    self.state = DETUMBLING
-                elif not self.ground_station_link_successful:
-                    self.state = STABLE
+        elif self.state == STABLE: # Cubessat has detumbled - Must establish comms link
+            if self.angular_velocity_stable and self.battery_check_successful and self.ground_station_link_successful:
+                self.state = MISSION
+            elif not self.battery_check_successful:
+                self.state = RECOVERY
+            elif not self.angular_velocity_stable:
+                self.state = DETUMBLING
+            elif not self.ground_station_link_successful:
+                self.state = STABLE
             
-            case MISSION: # Cubesat is performing mission
-                if self.angular_velocity_stable and self.battery_check_successful and self.ground_station_link_successful:
-                    self.state = MISSION
-                elif not self.battery_check_successful:
-                    self.state = RECOVERY
-                elif not self.angular_velocity_stable:
-                    self.state = DETUMBLING
-                elif not self.ground_station_link_successful:
-                    self.state = STABLE
+        elif self.state == MISSION: # Cubesat is performing mission
+            if self.angular_velocity_stable and self.battery_check_successful and self.ground_station_link_successful:
+                self.state = MISSION
+            elif not self.battery_check_successful:
+                self.state = RECOVERY
+            elif not self.angular_velocity_stable:
+                self.state = DETUMBLING
+            elif not self.ground_station_link_successful:
+                self.state = STABLE
                     
-            case RECOVERY:
-                pass
+        elif self.state == RECOVERY:
+            pass
 
 
